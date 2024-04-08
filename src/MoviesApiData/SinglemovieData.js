126f28343ac9df6aac4ed6051c6f1799e4ea0410
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-04-08 21:37:44 +0530

    Add: project setup

---
+++ src/MoviesApiData/SinglemovieData.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Movie from "./movie";
+
+function MovieData(){
+    return(
+        <>
+        </>
+    )
+}
+ export default MovieData
---
+++ src/homeComponent/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home (){
+    return (
+        <>
+        <h1>Good Morning!</h1>
+        <h3> i am a Home Page</h3>
+        </>
+    )
+}
+
+export default Home
