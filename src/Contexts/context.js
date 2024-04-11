6a1d314cbd128cb1ae81739d36170147e4453786
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-04-11 20:34:17 +0530

    adding API integration

--- src/Contexts/context.js
+++ src/Contexts/context.js
@@ -1,12 +1,13 @@
 import React, { useContext } from "react";
 
-
+const API_URl = 'http://www.omdbapi.com/?apikey=48aa2fca&s=titanic'
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-
     return (<AppContext.Provider value="PragyAni">{children}</AppContext.Provider>)
 }
+
+
 
 const useGlobalContext = () => {
     return (
