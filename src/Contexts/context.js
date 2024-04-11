a569ee5bcef3957c069e40d9a2dc7b95f1590aca
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-04-11 20:38:16 +0530

    add: useEffect for feteching data

--- src/Contexts/context.js
+++ src/Contexts/context.js
@@ -1,9 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext , useEffect } from "react";
 
 const API_URl = 'http://www.omdbapi.com/?apikey=48aa2fca&s=titanic'
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+
+    
+const TogetMovie = async (url) => {
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+useEffect(() => {
+    TogetMovie(API_URl);
+}, [])
+
     return (<AppContext.Provider value="PragyAni">{children}</AppContext.Provider>)
 }
 
