0869ff884758c5b37ef6be326fd3811c511c8b20
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-04-11 22:52:05 +0530

    Add: States and conditons

--- src/Contexts/context.js
+++ src/Contexts/context.js
@@ -1,28 +1,37 @@
-import React, { useContext , useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 const API_URl = 'http://www.omdbapi.com/?apikey=48aa2fca&s=titanic'
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
 
-    
-const TogetMovie = async (url) => {
-    try {
-        const res = await fetch(url);
-        const data = await res.json();
-        console.log(data);
-    } catch (error) {
-        console.log(error);
+    const [isLoading, setIsLoading] = useState(true)
+    const [movie, setMovie] = useState([])
+    const [iserror, setIserror] = useState({ show: "false", message: "" })
+    const TogetMovie = async (url) => {
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log(data);
+
+            if (data.Response == "True") {
+                setIsLoading(false)
+                setMovie(data.Search)
+            } else {
+                setIserror({ show: true, message: data.error })
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
-}
 
 
 
-useEffect(() => {
-    TogetMovie(API_URl);
-}, [])
+    useEffect(() => {
+        TogetMovie(API_URl);
+    }, [])
 
-    return (<AppContext.Provider value="PragyAni">{children}</AppContext.Provider>)
+    return (<AppContext.Provider value="PragyaAni">{children}</AppContext.Provider>)
 }
 
 
