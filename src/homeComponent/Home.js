33f00bc8ef3923ebe69ec34876e3bf884a145db3
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-04-11 22:54:59 +0530

    optimizing the erorr.

--- src/homeComponent/Home.js
+++ src/homeComponent/Home.js
@@ -3,14 +3,13 @@ import { useGlobalContext } from "../Contexts/context";
 // import { AppContext } from "../Contexts/context";
 
 function Home() {
-    // const name =useContext(AppContext)
-    const name = useGlobalContext();
+  
 
     return (
         <>
             <h1>Good Morning!</h1>
             <h3> i am a Home Page</h3>
-            <h2>{name}</h2>
+          
         </>
     )
 }
