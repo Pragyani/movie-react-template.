1e2a52dd8de925c546d2f32abfa1d82c1d907dce
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-04-12 20:37:28 +0530

    Adding JSX UI

--- src/MoviesApiData/movie.js
+++ src/MoviesApiData/movie.js
@@ -1,8 +1,30 @@
 import React from "react";
+import { useGlobalContext } from "../Contexts/context";
+import { NavLink } from "react-router-dom";
 
-function Movie (){
-    return(
+function Movie() {
+
+    const { movie } = useGlobalContext();
+    return (
         <>
+            <section className="movie-page">
+                <div className="grid grid-4-col">
+                      {movie.map((currVal)=>{
+
+                        const { imdbID , Title , Poster} = currVal
+                        
+                        return <NavLink to={`movie/${imdbID}`}>
+                            <div className="card">
+                                <div className="card-info">
+                                 <h2>{Title}</h2>
+                                 <img src={Poster} alt="logo"/>
+                                </div>
+                            </div>
+
+                        </NavLink>
+                      })}
+                </div>
+            </section>
         </>
     )
 }
