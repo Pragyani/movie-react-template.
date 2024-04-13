e94d8a0b63ea748ad349dd8551fcd0f829cf22bf
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-04-13 13:23:48 +0530

    adding key and chnage in classname

--- src/MoviesApiData/movie.js
+++ src/MoviesApiData/movie.js
@@ -8,12 +8,12 @@ function Movie() {
     return (
         <>
             <section className="movie-page">
-                <div className="grid grid-4-col">
+                <div className=" container grid grid-4-col">
                       {movie.map((currVal)=>{
 
                         const { imdbID , Title , Poster} = currVal
-                        
-                        return <NavLink to={`movie/${imdbID}`}>
+
+                        return <NavLink to={`movie/${imdbID}`} key={imdbID}>
                             <div className="card">
                                 <div className="card-info">
                                  <h2>{Title}</h2>
