c89042134d527937bc0c5d50cf7c96d4ee080671
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-04-14 10:57:09 +0530

    adding the error part

--- src/Serach.jsx
+++ src/Serach.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { useGlobalContext } from "./Contexts/context";
 
-function SearchBar (){
-    const {query, setQuery,iserror}=useGlobalContext();
-    return(
+function SearchBar() {
+    const { query, setQuery, iserror } = useGlobalContext();
+    return (
         <>
-        <section className="search-section">
-            <h2>Search the movie</h2> 
-            <form onSubmit={(e)=> e.preventDefault()}>
-                <div>
-                    <input type="text" placeholder="search here" value={query} onChange={(e)=> setQuery(e.target.value)}/>
+            <section className="search-section">
+                <h2>Search the movie</h2>
+                <form onSubmit={(e) => e.preventDefault()}>
+                    <div>
+                        <input type="text" placeholder="search here" value={query} onChange={(e) => setQuery(e.target.value)} />
+                    </div>
+                </form>
+                
+                <div className="card-error">
+                    <p>{iserror.show && iserror.message}</p>
                 </div>
-            </form>
-        </section>
+
+            </section>
         </>
     )
 }
